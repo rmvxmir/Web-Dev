fca602acff71dec537da3509e129d3968defd0a0
Amir Raimov <amirrmv@MacBook-Air-Amir-3.local>
2025-03-03 02:07:27 +0500

    Laboratory Work #6

---
+++ Lab 6/src/app/album-photos/album-photos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AlbumsService } from '../services/albums.service';
+
+@Component({
+    selector: 'app-photos',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './album-photos.component.html',
+    styleUrl: './album-photos.component.css'
+})
+export class AlbumPhotosComponent implements OnInit {
+    photos: any[] = [];
+    albumId!: number;
+
+    constructor(private route: ActivatedRoute, private albumsService: AlbumsService) {}
+
+    ngOnInit() {
+        this.albumId = Number(this.route.snapshot.paramMap.get('id'));
+
+        this.albumsService.getPhotosByAlbumId(this.albumId).subscribe(photos => {
+            this.photos = photos;
+        });
+    }
+}
